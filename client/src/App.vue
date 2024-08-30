e4a14672aeae63f39ec3233ad900b7c68bbdc1a4
jak2k <jakob@schwanenberg.name>
2024-08-30 18:15:00 +0200

    Update <head>

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,9 +3,9 @@
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
-  title: 'Vitesse',
+  title: 'Nimbus',
   meta: [
-    { name: 'description', content: 'Opinionated Vite Starter Template' },
+    { name: 'description', content: 'Realtime Collaboration Tool for Schools' },
     {
       name: 'theme-color',
       content: () => isDark.value ? '#00aba9' : '#ffffff',
